946154cd18b2cc0a5fbd7cb96b561cd34b4cb6b8
Rame <raja.email@example.com>
2026-06-06 07:13:13 +0000

    feat(terraform): add AWS provider configuration and license files

---
+++ s3-bucket-bootstrap/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-2" # Match the region you used in your main.tf backend
+}

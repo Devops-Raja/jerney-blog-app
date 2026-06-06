ef931cb8a4722bae175fee774971780130be19d1
Rame <raja.email@example.com>
2026-06-06 09:51:07 +0000

    feat(terraform): add helm release for AWS Load Balancer Controller

---
+++ terraform/helm-deploy.tf
@@ -0,0 +1,27 @@
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.9.0"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "module.load_balancer_controller_irsa.iam_role_arn"
+  }
+}
